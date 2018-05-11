4db30a48495c4dfbbfd2575fe7023e8e04728206
awad <awd@hotmail.it>
2018-05-11 17:29:19 +0200

    Add files via upload
    init

---
+++ include/status.h
@@ -0,0 +1,16 @@
+//
+// Created by awad on 10/05/18.
+//
+
+#ifndef BIRDBREEDER_STATUS_H
+#define BIRDBREEDER_STATUS_H
+
+namespace ba {
+    enum class BirdCycle {
+        chick, moulting , pairing, nestting , layyingEgg , hatching, raisingChicks,  ioslating ,
+        sick , healthy,unknown
+    };
+}
+
+
+#endif //BIRDBREEDER_STATUS_H
